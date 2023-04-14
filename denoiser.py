82b4c0d97d6aabda58b34d47c03ca59116926bed
Leon Plackal <110382152+leon-plackal@users.noreply.github.com>
2023-04-14 20:07:55 +1000

    added instructions

--- denoiser.py
+++ denoiser.py
@@ -6,17 +6,18 @@ from scipy import ndimage as nd
 import bm3d
 from skimage.restoration import (denoise_tv_chambolle)
 
-noisy = img_as_float(io.imread('images/veggies.jpg'))
+noisy = img_as_float(io.imread('results/veggies.jpg'))
 
+'''
+Adjust sigma_psd, radius and amount as needed to get desirable result.
+'''
 # BM3D
 BM3D_denoised = bm3d.bm3d(noisy, sigma_psd=0.17, stage_arg=bm3d.BM3DStages.ALL_STAGES)
 # sharpening
 result_3 = unsharp_mask(BM3D_denoised, radius=2, amount=2)
-plt.imsave('images/veggies_sharpened.png', result_3, cmap='gray')
+plt.imsave('results/veggies_sharpened.png', result_3, cmap='gray')
 
-# denoise TV
-# denoise_TV = denoise_tv_chambolle(noisy, weight=0.3)
-# plt.imsave('images/TV_smoothed2.png', denoise_TV, cmap='gray')
+
 #guassian denoise
 # guassian_img = nd.gaussian_filter(noisy, sigma=3)
 # plt.imsave('images/guass_smooth.png', guassian_img, cmap='gray')
